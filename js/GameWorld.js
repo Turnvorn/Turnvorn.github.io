f6a4a9485d7947f2512b3999e3141f074a052280
ViktorMS <vjh2@hi.is>
2021-06-02 20:20:33 +0000

    Pása í byrjun

--- js/GameWorld.js
+++ js/GameWorld.js
@@ -209,6 +209,7 @@ class GameWorld {
     this.levelSeconds += secondsPassed;
     this.oldTimeStamp = timeStamp;
 
+    // Todo remove with play button
     if (this.level === 1) {
       this.spawnLevel_1();
     }
@@ -267,7 +268,7 @@ class GameWorld {
       this.context.fill();
       // Spot
       this.context.beginPath();
-      this.context.fillStyle = "rgba(75, 150, 75, 1)";
+      this.context.fillStyle = "rgba(50, 175, 100, 1)";
       this.context.arc(ele[0], ele[1], 35, 0, 2 * Math.PI);
       this.context.fill();
     });
@@ -295,21 +296,26 @@ class GameWorld {
     }
     // 64x36
     this.start = { x: 0, y: 400 };
-    var i;
-    for (i = 0; i < 20; i++) {
-      var random = Math.floor(Math.random() * 50) + 1;
-      this.spawnEnemy(
-        i,
-        i / 3,
-        new Enemy(
-          this.context,
-          this.start.x - random,
-          this.start.y,
-          64 * 2,
-          0,
-          1
-        )
-      );
+    // Todo remove with play button
+    if (this.towers.length > 0) {
+      var i;
+      for (i = 0; i < 15; i++) {
+        var random = Math.floor(Math.random() * 50) + 1;
+        this.spawnEnemy(
+          i,
+          i / 3,
+          new Enemy(
+            this.context,
+            this.start.x - random,
+            this.start.y,
+            64 * 2,
+            0,
+            1
+          )
+        );
+      }
+    } else {
+      this.levelSeconds = 0; // Round not begun
     }
   }
 
